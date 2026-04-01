f573cc0d2f1439fde36f729eb558a64e2fcfbb75
Stefan Hermann <stefan.a.hermann@gmail.com>
2026-04-01 15:28:15 +0200

    bench_tiny.cpp

--- benchmarks/bench_tiny.cpp
+++ benchmarks/bench_tiny.cpp
@@ -1,92 +1,91 @@
-#include <benchmark/benchmark.h>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <set>
 #include <random>
+#include <chrono>
 
 #include "deli.h"
 #include "veb.h"
 #include "rht.h"
+#define XXH_PRIME64_2  0xC2B2AE3D27D4EB4FULL  /*!< 0b1100001010110010101011100011110100100111110101001110101101001111 */
+#define XXH_PRIME64_3  0x165667B19E3779F9ULL  /*!< 0b0001011001010110011001111011000110011110001101110111100111111001 */
 
-constexpr size_t NUM_KEYS = 1 << 28;
-constexpr size_t NUM_QUERIES = 1 << 20;
-constexpr size_t low_bits = 20;
-
-template <typename Index>
-void bench_insert(Index &index, const char * index_name, std::vector<int> & keys, std::vector<int> & queries) {
-  auto bench_func = [&](::benchmark::State& state) {
-    size_t i = 0;
-    for (auto _ : state) {
-      index.insert(keys[i]);
-      i = (i == NUM_KEYS - 1) ? 0 : i + 1;
-    }
-    state.counters["used_keys"] = index.size();
-    state.counters["available_keys"] = NUM_KEYS;
-  };
-  benchmark::RegisterBenchmark(index_name, bench_func);
+static uint64_t XXH64_avalanche(uint64_t hash) {
+    hash ^= hash >> 33;
+    hash *= XXH_PRIME64_2;
+    hash ^= hash >> 29;
+    hash *= XXH_PRIME64_3;
+    hash ^= hash >> 32;
+    return hash;
 }
 
-template <typename Index>
-void bench_query(Index &index, const char * index_name, std::vector<int> & keys, std::vector<int> & queries) {
-  auto bench_func = [&](::benchmark::State& state) {
-    using T = typename Index::value_type;
-    static size_t i_ = 0;
-    T sum;
+constexpr bool test = false;
+
+using T = uint32_t;
+
+template<typename Index>
+void bench_query(Index &index, std::vector<uint64_t> &keys, std::vector<uint64_t> &queries) {
+    std::set<T> test_set;
+    T sum = 0;
+    if constexpr (test) {
+        test_set.insert(keys.begin(), keys.end());
+    }
     index.bulk_load(keys.begin(), keys.end());
-    for (auto _ : state) {
-      auto result = index.find_next(queries[i_] ^ (sum & 0x01));
-#ifndef NDEBUG
-      auto lb = std::lower_bound(keys.begin(), keys.end(), queries[i_] ^ (sum & 0x01));
-      if (result && lb == keys.end()) {
-        std::cout << "Error: expected null optional; got: " << result.value() << std::endl;
-        throw std::runtime_error("Incorrect result from find_next");
-      }
-      if (result != (lb == keys.end() ? -1 : *lb)) {
-        std::cout << "Error: expected " << *lb << "; got: " << result.value() << std::endl;
-        throw std::runtime_error("Incorrect result from find_next");
-      }
-#endif
-      sum += result.value();
-      i_ = (i_ == NUM_QUERIES - 1) ? 0 : i_ + 1;
+
+    using clock = std::chrono::steady_clock;
+    auto start = clock::now();
+    for (auto q:queries) {
+        auto result = index.find_next(q);
+        if constexpr (test) {
+            auto it_next = test_set.lower_bound(q);
+            if (it_next != test_set.end()) {
+                T expected_next = *it_next;
+                if(result != expected_next){
+                    exit(1);
+                }
+            } else {
+                if(result != std::nullopt) {
+                    exit(1);
+                }
+            }
+        }
+        sum += result.value_or(0);
     }
-    state.counters["queried_keys"] = i_;
-    state.counters["available_keys"] = NUM_KEYS;
-    state.counters["dno"] = sum;
-  };
-  benchmark::RegisterBenchmark(index_name, bench_func);
+    auto end = clock::now();
+
+    auto elapsed_ns =
+            std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    std::cout << static_cast<double>(elapsed_ns) / queries.size() << " " << sum << std::endl;
 }
 
-int main(int argc, char** argv) {
-  // Generate a vector of keys
-  std::vector<int> keys(NUM_KEYS), queries(NUM_QUERIES);
-  for (size_t i = 0; i < NUM_KEYS; ++i) {
-    keys[i] = i * 2654435761 % NUM_KEYS;
-  }
-  for (size_t i = 0; i < NUM_QUERIES; ++i) {
-    queries[i] = i * 40503 % NUM_QUERIES;
-  }
+int main(int argc, char **argv) {
+    constexpr size_t NUM_QUERIES = 1 << 20;
+    constexpr size_t bits = 10;
+    constexpr uint64_t mask = (uint64_t(1)<<bits)-1;
+    // Generate a vector of keys
+    for (uint64_t log_keys = 7; log_keys < 24; ++log_keys) {
+        std::cout<<log_keys<<" ";
+        uint64_t NUM_KEYS = uint64_t(1)<<log_keys;
+        std::vector<uint64_t > keys(NUM_KEYS), queries(NUM_QUERIES);
+        for (size_t i = 0; i < NUM_KEYS; ++i) {
+            keys[i] = XXH64_avalanche(i) & mask;
+        }
+        for (size_t i = 0; i < NUM_QUERIES; ++i) {
+            queries[i] = XXH64_avalanche(~i) & mask;
+        }
 
-  std::shuffle(queries.begin(), queries.end(), std::mt19937{0});
+        // Sorted keys for the queries
+        std::vector<uint64_t> sorted_keys;
+        std::copy(keys.begin(), keys.end(), std::back_inserter(sorted_keys));
+        std::sort(sorted_keys.begin(), sorted_keys.end());
 
-  // Sorted keys for the queries
-  std::vector<int> sorted_keys;
-  std::copy(keys.begin(), keys.end(), std::back_inserter(sorted_keys));
-  std::sort(sorted_keys.begin(), sorted_keys.end());
+        using Index = DeLI::RHT<true, bits, DeLI::RhtOptimization::none, 4, 50>;
+        //using Index = DeLI::DeLI<true, DeLI::RhtOptimization::none, 5, 50, DeLI::TopLevelOptimization::none, T , 16, bits >;
+        Index rht_index;
+        //DeLI::utils::print_simd_info<Index::Tvec >();
 
-  DeLI::DeLI<int, low_bits> deli_index;
-  bench_insert(deli_index, "DeLI Insert", keys, queries);
-  bench_query(deli_index, "DeLI Query", sorted_keys, queries);
-
-  // veb_tree veb_index;
-  // bench_insert(veb_index, "VEB Insert", keys, queries);
-  // bench_query(veb_index, "VEB Query", sorted_keys, queries);
-
-  DeLI::RHT<int> rht_index(0, std::numeric_limits<int>::max());
-  bench_insert(rht_index, "RHT Insert", keys, queries);
-  bench_query(rht_index, "RHT Query", sorted_keys, queries);
-
-  benchmark::Initialize(&argc, argv);
-  benchmark::RunSpecifiedBenchmarks();
-  benchmark::Shutdown();
+        bench_query(rht_index, sorted_keys, queries);
+    }
 }
